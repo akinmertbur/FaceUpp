3a65b64c3b3f22f56d2e72c0b586241333596de3
Akin Mert Bur <akinmertbur@gmail.com>
2024-07-04 18:40:12 +0300

    Fix the conflicts

--- src/presentation/controllers/userController.js
+++ src/presentation/controllers/userController.js
@@ -6,9 +6,9 @@ const createUser = async (req, res) => {
     const user = await addUser(req.body);
     log(`User created with ID: ${user.id}`); // Log a message when user is created
     res.status(201).redirect("/");
-  } catch (error) {
+  } catch (err) {
     error(`Failed to create user: ${err.message}`); // Log an error message if there's an exception
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: err.message });
   }
 };
 
